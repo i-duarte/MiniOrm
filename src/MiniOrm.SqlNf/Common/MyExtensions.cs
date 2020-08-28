0e87da8cfcd25e8f41df40608607c695e33911b9
IVAN DUARTE BRAVO <i_duarte@hotmail.com>
2020-08-27 18:56:26 -0700

    restructuracion

---
+++ src/MiniOrm.SqlNf/Common/MyExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Data;
+
+namespace MiniOrm.Common
+{
+	public static class MyExtensions
+	{
+		public static IEnumerable<string> GetColumnNames(
+			this IDataReader source
+		)
+		{
+			for (var i = 0; i < source.FieldCount; i++)
+			{
+				yield return source.GetName(i);
+			}
+		}
+
+		
+	}
+}
---
+++ src/MiniOrm.SqlNf/Common/Parameter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace MiniOrm.Common
+{
+	public class Parameter
+	{
+		public string Name { get; set; }
+		public object Value { get; set; }
+	}
+}
